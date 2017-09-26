81f53f7917a0b59b45acea8782dd0d7d67fc2646
Sebastian Bosek <sebastian.bosek.13@cnu.edu>
2017-09-26 19:26:59 -0400

    Add files via upload

--- Name.java
+++ Name.java
@@ -1,5 +1,3 @@
-import java.util.Date;
-
 public class Name {
     private String title;
     private String first;
@@ -10,16 +8,9 @@ public class Name {
     private String suffixGenerational; // e.g., III, Jr., Sr., etc.
     private String suffixProfessional; // e.g., Ph.D., CPA, etc.
 
-    public Name (
-                 String title,
-                 String first,
-                 String middle,
-                 String surname,
-                 String surnameParent1,
-                 String surnameParent2,
-                 String generational,
-                 String professional
-                 ) {
+    public Name(String title, String first, String middle, String surname,
+            String surnameParent1, String surnameParent2, String generational,
+            String professional) {
         this.title = title;
         this.first = first;
         this.middle = middle;
@@ -31,34 +22,36 @@ public class Name {
     }
 
     public String getTitle() {
-        /* return name's title */
+        return title;
     }
 
     public String getFirst() {
-        /* return Name's first name */
+        return first;
     }
 
     public String getMiddle() {
-        /* return Name's middle name */
+        return middle;
     }
 
     public String getSurname() {
-        /* return Name's last name */
+        return surname;
     }
 
     public String getSurnameParent1() {
-        /* return parent1's last name */
+        return surnameParent1;
     }
 
     public String getSurnameParent2() {
-        /* return parent2's last name */
+        return surnameParent2;
     }
 
     public String getSuffixProfessional() {
-    	/* return Name's professional suffix */
+        return suffixProfessional;
     }
 
     public String getSuffixGenerational() {
-        /* return Name's generational suffix */
-}
+        return suffixGenerational;
+    }
+
+  
 }
